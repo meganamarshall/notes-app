c432d227bda1b90bd1651e7e2c89bd69b0132e07
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-10 20:31:50 -0700

    put all notes in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import CreateNote from '../containers/notes/CreateNote';
+import AllNotes from '../containers/notes/AllNotes';
 
 export default function App() {
-  return <CreateNote />;
+  return (
+  <>
+  <CreateNote />
+  <AllNotes />
+  </>
+  );
 }
-  

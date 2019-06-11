c8f1dfff4b034a5cf7dc047f4a83b91725dbe5e4
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-11 13:42:24 -0700

    withSession container component

---
+++ src/containers/auth/withSession.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { login } from '../../services/auth';
+import { connect } from 'redux';
+import { getToken } from '../../selectors/sessionSelector';
+
+
+export const withSession = Component => {
+  class WithSession extends PureComponent {
+    static propTypes = {
+      token: PropTypes.string.isRequired
+    }
+
+    componentDidMount() {
+      if(!this.props.token) {
+        login();
+      }
+    }
+
+    render() {
+      if(!this.props.token) return <h1>LoADING</h1>;
+      return <Component />;
+    }
+  }
+  const mapStateToProps = state => {
+    token: getToken(state);
+  };  
+  return connect(
+    mapStateToProps
+  )(WithSession);
+};

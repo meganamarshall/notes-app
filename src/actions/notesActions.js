7be7ce5d7dae604e2155300490ab3f0f46786789
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-10 17:39:01 -0700

    adding fetching notes

--- src/actions/notesActions.js
+++ src/actions/notesActions.js
@@ -6,3 +6,9 @@ export const [
   NEW_NOTE,
   NEW_NOTE_PENDING
 ] = createAction('NEW_NOTE', createNote);
+
+export const [
+  fetchNotes,
+  FETCH_NOTES,
+  FETCH_NOTES_PENDING
+] = createAction('FETCH_NOTES', getNotes);
--- src/services/noteApi.js
+++ src/services/noteApi.js
@@ -1,4 +1,6 @@
 import { post } from './request';
+import { get } from 'http';
 
 export const createNote = note => post('/api/v1/notes', note);
+export const getNotes = () => get('/api/v1/notes')
 

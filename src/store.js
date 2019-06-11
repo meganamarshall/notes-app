badef4a991f1195938230fda273234f943136c76
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-11 09:20:34 -0700

    fix to store

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { promiseMiddleware } from 'promise-middleware-redux';
 import reducer from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   reducer,

0bb10e496e25e38e7f3035e862b0efcc08f2b05e
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-11 13:48:19 -0700

    updated app with route/switch/home/callback

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import CreateNote from '../containers/notes/CreateNote';
-import AllNotes from '../containers/notes/AllNotes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+}  from 'react-router-dom';
+import Home from './Home';
+import Callback from '../containers/auth/Callback';
+import { withSession } from '../containers/auth/withSession';
 
 export default function App() {
   return (
-  <>
-  <CreateNote />
-  <AllNotes />
-  </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={withSession(Home)} />
+        <Route path="/callback" component={Callback} />
+      </Switch>
+    </Router>
   );
 }
